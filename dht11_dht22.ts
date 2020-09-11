2bebe3abaafa2f9b6a8b93113982c69071e34b3c
James <51168875+James-calliope@users.noreply.github.com>
2020-09-11 16:31:03 +0800

    Update dht11_dht22.ts

--- dht11_dht22.ts
+++ dht11_dht22.ts
@@ -2,7 +2,7 @@
  * MakeCode editor extension for DHT11 and DHT22 humidity/temperature sensors
  * by Alan Wang
  */
-//% block="DHT11/DHT22" weight=100 color=#ff8f3f icon="\uf043"
+//% block="DHT11/DHT22温湿度传感器" weight=100 color=#ff8f3f icon="\uf043"
 namespace dht11_dht22 {
 
     let _temperature: number = -999.0
